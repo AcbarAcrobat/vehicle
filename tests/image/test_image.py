f201545c2a35408f6574b10146caa28e7a82ce51
Konstantin <kdavleev@at-consulting.ru>
2020-08-17 15:58:31 +0700

    image

---
+++ tests/image/test_image.py
@@ -0,0 +1,43 @@
+import pytest
+import allure
+from helper import LOGGER
+from truth.truth import AssertThat
+from endpoint.image import Image
+import numpy
+
+
+class TestImage:
+
+    def test_image_upload(self, faker, session):
+        i = Image(session)
+        image = bytes(numpy.random.rand(30,30,3) * 255)
+        r = i.upload(image, faker.uuid4(), "image/png").json()
+        """
+        {
+            "link": "/image/get?token=null&id=295424",
+            "result": 295424,
+            "done": true
+        }
+        """
+        LOGGER.info(r)
+        AssertThat(r["result"]).IsInstanceOf(int)
+        AssertThat(r["done"]).IsTrue()
+
+    def test_image_get(self, faker, session):
+        """
+        request url: /image/get
+        request body : {"id": int}
+        answer: b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x04\xa8\x00\x00\x02\xa0\x08\x02\x00\x00\x000<\xd2%\x00\x00'
+        """
+        i = Image(session)
+        file_name = faker.uuid4()
+        image = bytes(numpy.random.rand(30,30,3) * 255)
+
+        r = i.upload(image, file_name, "image/png").json()
+        LOGGER.info(r)
+        id_ = r["result"]
+
+        r = i.get(id_)
+        LOGGER.info(r.headers)
+        AssertThat(r.content).IsEqualTo(image)
+        AssertThat(r.headers["Content-Disposition"]).Contains(file_name)
